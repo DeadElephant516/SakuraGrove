432ab7f5f5902e4dc326383d81580d502c87ff47
markindo20 <markindogamer@gmail.com>
2025-07-13 14:53:55 +0200

    added full screen/ windowse options and SFX to it
    i added music to the main menu, sfx when clicking and full screen options/ windowed mode for the projecst

--- Scenes/main menu/main_menu.gd
+++ Scenes/main menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 @onready var main_buttons: VBoxContainer = $MainButtons
 @onready var options: Panel = $Options
-
+@onready var click_sfx: AudioStreamPlayer2D = $"click SFX"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,17 +17,21 @@ func _ready():
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/TestScene/test_scene.tscn")
+	click_sfx.play()
 
 
 func _on_settings_2_pressed() -> void:
 	print("settings pressed")
 	main_buttons.visible = false
 	options.visible = true
+	click_sfx.play()
 
 
 func _on_exit_3_pressed() -> void:
 	get_tree().quit()
+	click_sfx.play()
 
 
 func _on_back_options_pressed() -> void:
 	_ready()
+	click_sfx.play()
